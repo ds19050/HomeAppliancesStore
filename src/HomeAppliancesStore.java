59578faa1a0d54bd1c4fdd6e6082b23d91718336
ds19050 <johnkillerboy27@gmail.com>
2021-01-22 15:17:22 +0200

    commit HomeAppliances Askisi4

--- src/HomeAppliancesStore.java
+++ src/HomeAppliancesStore.java
@@ -46,6 +46,27 @@ public class HomeAppliancesStore {
                 + " Διεύθυνση εταιρείας: " + CompanyAddress +
                 " Αριθμός Υπαλλήλων: " + NumberOfEmployees);
 
+        Fridge fridge1=new Fridge("LG","LG wid9","fridge","freon");
+        Fridge fridge2=new Fridge("SONY","sonya22","fridge","freon");
+        Fridge fridge3=new Fridge();
+        AirCondition ac1=new AirCondition("Whirlpool","infrared");
+        AirCondition ac2=new AirCondition("Samsung","wifi");
+        AirCondition ac3=new AirCondition("LG","infrared");
+        AirCondition ac4=new AirCondition("LG","wifi");
+        Oven oven1=new Oven("Pitsos","Pitsos1234","oven fast",7);
+        WashingMachine washingMachine1=new WashingMachine();
+        WashingMachine washingMachine2=new WashingMachine();
+        WashingMachine washingMachine3=new WashingMachine("Logitech","LG Wash plus",
+                "wash machine",92000);
+
+        System.out.println(ac1.toString());
+        System.out.println(ac2.toString());
+        System.out.println(fridge1.toString());
+        System.out.println("Fridges:"+Fridge.getDeviceCount());
+        System.out.println("AirCondition:"+AirCondition.getDeviceCount());
+        System.out.println("Oven:"+Oven.getDeviceCount());
+        System.out.println("Oven:"+Oven.getDeviceCount());
+
 
     }
 }
